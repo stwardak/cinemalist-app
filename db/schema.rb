e71e25730f8454d1d2169b049c8130e9d1578abd
stwardak <148924249+stwardak@users.noreply.github.com>
2024-02-02 14:52:25 -0600

    add category model and seeds with sample data

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_02_190354) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_02_204809) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "directors", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,56 @@
 # Director Seeds
-Director.create!(name:"Alfred Hitchcock")
+  # Director.create!(name:"Alfred Hitchcock")
 
-Director.create!(name:"Stanley Kubrick")
+  # Director.create!(name:"Stanley Kubrick")
 
-Director.create!(name:"Quentin Tarantino")
+  # Director.create!(name:"Quentin Tarantino")
 
-Director.create!(name:"Darren Aronofsky")
+  # Director.create!(name:"Darren Aronofsky")
 
-Director.create!(name:"Greta Gerwig")
+  # Director.create!(name:"Greta Gerwig")
 
-Director.create!(name:"Steven Spielberg")
+  # Director.create!(name:"Steven Spielberg")
 
-Director.create!(name:"Tim Burton")
+  # Director.create!(name:"Tim Burton")
 
-Director.create!(name:"Ridley Scott")
+  # Director.create!(name:"Ridley Scott")
 
-Director.create!(name:"Wes Anderson")
+  # Director.create!(name:"Wes Anderson")
 
-Director.create!(name:"Victor Fleming")
+  # Director.create!(name:"Victor Fleming")
 
-Director.create!(name:"Sophia Coppola")
+  # Director.create!(name:"Sophia Coppola")
 
-Director.create!(name:"Martin Scorsese")
+  # Director.create!(name:"Martin Scorsese")
 
-Director.create!(name:"Christopher Nolan")
+  # Director.create!(name:"Christopher Nolan")
 
-Director.create!(name:"Roman Polanski")
+  # Director.create!(name:"Roman Polanski")
 
-Director.create!(name:"Bong Joon Ho")
+  # Director.create!(name:"Bong Joon Ho")
 
-Director.create!(name:"Spike Lee")
+  # Director.create!(name:"Spike Lee")
 
-Director.create!(name:"David Lynch")
+  # Director.create!(name:"David Lynch")
 
-Director.create!(name:"Kevin Smith")
+  # Director.create!(name:"Kevin Smith")
 
 # Movie Seeds
 
 # Category Seeds
+ Category.create!(genre:"action")
+ Category.create!(genre:"adventure")
+ Category.create!(genre:"animation")
+ Category.create!(genre:"comedy")
+ Category.create!(genre:"crime")
+ Category.create!(genre:"documentary")
+ Category.create!(genre:"drama")
+ Category.create!(genre:"fantasy")
+ Category.create!(genre:"historical")
+ Category.create!(genre:"horror")
+ Category.create!(genre:"musical")
+ Category.create!(genre:"noir")
+ Category.create!(genre:"romance")
+ Category.create!(genre:"sci-fi")
+ Category.create!(genre:"thriller")
+ Category.create!(genre:"western")
