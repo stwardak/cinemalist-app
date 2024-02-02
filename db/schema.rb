b3b58eb95eb9e49b3563a431c0571de05d5fe64c
stwardak <148924249+stwardak@users.noreply.github.com>
2024-02-02 14:41:46 -0600

    add director model and seeds with sample dataa

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_02_185250) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_02_190354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "directors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Director Seeds
+Director.create!(name:"Alfred Hitchcock")
+
+Director.create!(name:"Stanley Kubrick")
+
+Director.create!(name:"Quentin Tarantino")
+
+Director.create!(name:"Darren Aronofsky")
+
+Director.create!(name:"Greta Gerwig")
+
+Director.create!(name:"Steven Spielberg")
+
+Director.create!(name:"Tim Burton")
+
+Director.create!(name:"Ridley Scott")
+
+Director.create!(name:"Wes Anderson")
+
+Director.create!(name:"Victor Fleming")
+
+Director.create!(name:"Sophia Coppola")
+
+Director.create!(name:"Martin Scorsese")
+
+Director.create!(name:"Christopher Nolan")
+
+Director.create!(name:"Roman Polanski")
+
+Director.create!(name:"Bong Joon Ho")
+
+Director.create!(name:"Spike Lee")
+
+Director.create!(name:"David Lynch")
+
+Director.create!(name:"Kevin Smith")
+
+# Movie Seeds
+
+# Category Seeds
