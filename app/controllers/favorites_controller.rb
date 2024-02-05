7690213515eea49781cb8e48b1d216daf5409978
stwardak <148924249+stwardak@users.noreply.github.com>
2024-02-05 13:47:09 -0600

    create, index, and destroy actions for favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,13 +6,26 @@ class FavoritesController < ApplicationController
     render :index
   end
 
-  # def create
-  #   @favorite = Favorite.create(
-  #     user_id: current_user.id,
-  #     movie_id: params[:movie_id]
-  #   )
-  #   render :show
-  # end
+  def create
+    if Favorite.exists?(user_id: current_user, movie_id:params[:movie_id])
+      rendor json: { message: "Movie has already been added to favorites" }
+    else
+      @favorite = Favorite.create(
+        user_id: current_user.id,
+        movie_id: params[:movie_id]
+      )
+      render :show
+    end
+  end
 
+  def destroy
+    @favorite = Favorite.find_by(id: params[:id])
+    if @favorite.present? && @favorite.user_id == current_user.id
+      @favorite.destroy
+      render json: {message: "Movie has been removed from your favorites"}
+    else
+      render json: {message: "Movie not found in current user's favorites"}
+    end
+  end
 
 end
