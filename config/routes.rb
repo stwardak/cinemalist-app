53f2309e5b5eaa73a121dc63eb8395ab305cb1f3
stwardak <148924249+stwardak@users.noreply.github.com>
2024-02-05 11:29:22 -0600

    added user story comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,44 +6,44 @@ Rails.application.routes.draw do
   get "/movies/:id" => "movies#show" # a user can view a specific movie
   # post "/movies" => "movies#create" # will only need for admin
   # patch "/movies/:id" => "movies#update" # will only need for admin
-  # delete "/movies/:id" => "movies#destroy" # shouldn't need
+  # delete "/movies/:id" => "movies#destroy" # shouldn't need, unless for admin purposes
 
 
   # User Routes
-  # get "/users" => "users#index"
-  # get "/users/:id" => "users#show"
+  # get "/users" => "users#index" # view list of all users
+  # get "/users/:id" => "users#show" # view specific user
   post "/users" => "users#create" # create a new user / a user can sign up
   patch "/users/:id" => "users#update" # user can edit their login information and settings
-  # delete "/users/:id" => "users#destroy"
+  # delete "/users/:id" => "users#destroy" # allow users to delete their account
 
 #   # Director Routes
-#   get "/directors" => "directors#index"
-#   get "/directors/:id" => "directors#show"
-#   post "/directors" => "directors#create"
-#   patch "/directors/:id" => "directors#update"
-#   delete "/directors/:id" => "directors#destroy"
+#   get "/directors" => "directors#index" # a user can view all directors
+#   get "/directors/:id" => "directors#show" # a user can view a specific director
+#   post "/directors" => "directors#create" # shouldn't need, unless for admin purposes
+#   patch "/directors/:id" => "directors#update" # shouldn't need, unless for admin purposes
+#   delete "/directors/:id" => "directors#destroy" # shouldn't need, unless for admin purposes
 
 #   # Category Routes
-#   get "/categories" => "categories#index"
-#   get "/categories/:id" => "categories#show"
-#   post "/categories" => "categories#create"
-#   patch "/categories/:id" => "categories#update"
-#   delete "/categories/:id" => "categories#destroy"
+#   get "/categories" => "categories#index" # a user can browse movies by category
+#   get "/categories/:id" => "categories#show" # a user can view all movies in a category
+#   post "/categories" => "categories#create" # shouldn't need, unless for admin purposes
+#   patch "/categories/:id" => "categories#update" # shouldn't need, unless for admin purposes
+#   delete "/categories/:id" => "categories#destroy" # shouldn't need, unless for admin purposes
 
 #   # Favorite Routes
-#   get "/favorites" => "favorites#index"
-#   get "/favorites/:id" => "favorites#show"
-#   post "/favorites" => "favorites#create"
-#   patch "/favorites/:id" => "favorites#update"
-#   delete "/favorites/:id" => "favorites#destroy"
+#   get "/favorites" => "favorites#index" # a user can view their favorites
+#   get "/favorites/:id" => "favorites#show" # a user can view details for one of their favorites 
+#   post "/favorites" => "favorites#create" # a user can add a movie to their favorites
+#   patch "/favorites/:id" => "favorites#update" # shouldn't need, unless for admin purposes
+#   delete "/favorites/:id" => "favorites#destroy" # a user can remove a movie from their favorites
 
 
 #   # Category_Movie Routes
-#   get "/category_movies" => "category_movies#index"
-#   get "/category_movies/:id" => "category_movies#show"
-#   post "/category_movies" => "category_movies#create"
-#   patch "/category_movies/:id" => "category_movies#update"
-#   delete "/category_movies/:id" => "category_movies#destroy"
+#   get "/category_movies" => "category_movies#index" # shouldn't need, unless for admin purposes
+#   get "/category_movies/:id" => "category_movies#show" # shouldn't need, unless for admin purposes
+#   post "/category_movies" => "category_movies#create" # shouldn't need, unless for admin purposes
+#   patch "/category_movies/:id" => "category_movies#update" # shouldn't need, unless for admin purposes
+#   delete "/category_movies/:id" => "category_movies#destroy" # shouldn't need, unless for admin purposes
 
   # Sessions Routes
   post "/sessions" => "sessions#create" # user login session
